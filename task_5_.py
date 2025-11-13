52a9418427f0f736ae10c0d38976e864879d9c6f
aeksei <aeksei94@gmail.com>
2025-11-13 19:46:22 +0300

    задача 5_

--- task_5_.py
+++ task_5_.py
@@ -6,9 +6,18 @@
 Напишите программу, которая подсчитывает сумму расходов за неделю.
 Проверьте с помощью `assert` результат
 """
+from task_5 import total_expense
 
 expenses = [
     [150, 300, 50, 400, 250, 100, 500],
     [250, 100, 450, 200, 550, 150, 550]
 ]  # расходы на продукты в течение недели
 
+total_expenses = 0
+for week_expenses in expenses:
+    total_expenses += sum(week_expenses)
+
+print(total_expenses)
+#
+# print(sum(expenses, start=[]))
+# print(sum(sum(expenses, start=[])))
