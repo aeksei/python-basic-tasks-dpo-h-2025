19cf12ebe94addc7e3b14103789bc9e9b9266130
aeksei <aeksei94@gmail.com>
2025-11-17 20:56:30 +0300

    refactor: использование list comprehension

--- task_2_1.py
+++ task_2_1.py
@@ -26,6 +26,10 @@ def get_product_price_with_discount(products: list[dict]) -> list[float]:
         price_with_discount = product["price"] * (1 - product["discount"] * 0.01)
         prices.append(price_with_discount)
 
+    prices = [
+        product["price"] * (1 - product["discount"] * 0.01) for product in products
+    ]
+
     return prices
 
 
