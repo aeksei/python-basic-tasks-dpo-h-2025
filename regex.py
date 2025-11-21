3deef54edf316a385a9d70f4b8379aa680a4e1ae
aeksei <aeksei94@gmail.com>
2025-11-21 21:23:48 +0300

    refactor: удалён закоментированный код

--- regex.py
+++ regex.py
@@ -31,18 +31,3 @@ if __name__ == "__main__":
                     )
                 else:
                     print(text_date, "Некорректная дата")
-
-
-# matches = re.finditer(regex, test_str, re.MULTILINE)
-#
-# for matchNum, match in enumerate(matches, start=1):
-#
-#     print("Соответствие {matchNum} было найдено в {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-#                                                                                  end=match.end(), match=match.group()))
-#
-#     for groupNum, _ in enumerate(match.groups(), start=1):
-#
-#         print(
-#             "Группа {groupNum} найдена в {start}-{end}: {group}".format(groupNum=groupNum, start=match.start(groupNum),
-#                                                                         end=match.end(groupNum),
-#                                                                         group=match.group(groupNum)))
